b66c008af695f7b6cd33a76961d9874bda0327d7
JinchengHeRyan <delteforce@outlook.com>
2022-04-11 17:53:01 +0800

    Add argparse function

--- spotifyCrawl.py
+++ spotifyCrawl.py
@@ -1,25 +1,13 @@
 import spotipy
+import os
+import time
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 from tqdm import tqdm
 from util.utils import *
 from dataloader import csvTrackIdLoader
+import argparse
 
-# input files path
-input_csv = "./data/smallSet/data.csv"
-output_csv = "./out_data/smallSet/outData.csv"
-
-csv_input = pd.read_csv(input_csv)
-outFile = open(output_csv, "w")
-
-spotify = spotipy.Spotify(
-    client_credentials_manager=SpotifyClientCredentials(
-        client_id="2f088e29c62846a3975616f763269566",
-        client_secret="bff923971e354eb4b13499a1b1d67d14",
-    ),
-    # retries=1000000,
-    requests_timeout=100,
-)
 
 metric_space = [
     "trackID",
@@ -39,8 +27,69 @@ metric_space = [
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Spotify Data Crawler")
+    parser.add_argument(
+        "--inputCSV",
+        default="./data/smallSet/data.csv",
+        type=str,
+        required=False,
+        help="input csv file which contains track IDs",
+    )
+    parser.add_argument(
+        "--outputDir",
+        default="./out_data/",
+        type=str,
+        required=False,
+        help="output directory of the crawled data",
+    )
+    parser.add_argument(
+        "--batchsize",
+        default=50,
+        type=int,
+        required=False,
+        help="batch size you want to crawl the data, 50 is recommended and is the maximum of being supported",
+    )
+    parser.add_argument(
+        "--clientID",
+        default="2f088e29c62846a3975616f763269566",
+        type=str,
+        required=False,
+        help="client ID of your spotify development account",
+    )
+    parser.add_argument(
+        "--clientSecret",
+        default="bff923971e354eb4b13499a1b1d67d14",
+        type=str,
+        required=False,
+        help="client secret of your spotify development account",
+    )
+    args = parser.parse_args()
+
+    spotify = spotipy.Spotify(
+        client_credentials_manager=SpotifyClientCredentials(
+            client_id=args.clientID,
+            client_secret=args.clientSecret,
+        ),
+        # retries=1000000,
+        requests_timeout=100,
+    )
+
+    # Assert the input csv file
+    assert os.path.isfile(args.inputCSV), "No such input csv file: %s" % args.inputCSV
+
+    # Create output csv file
+    os.makedirs(args.outputDir, exist_ok=True)
+    output_csv = os.path.join(
+        args.outputDir, time.strftime("%Y-%m-%d-%H%M.csv", time.localtime(time.time()))
+    )
+    outFile = open(output_csv, "w")
+
+    # input files path
+    input_csv = args.inputCSV
+    csv_input = pd.read_csv(input_csv)
+
     writeMetric_Name(outFile, metric_space)
-    batchsize = 50
+    batchsize = args.batchsize
     id_loader = csvTrackIdLoader(csvPath=input_csv, batchSize=batchsize)
 
     for i, trackIDs in enumerate(tqdm(id_loader, ncols=80)):
